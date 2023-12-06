79dbc080512535813876ccbb455bb1d8e6c6af31
ricky-kiva <ricky.kiva@gmail.com>
2023-12-06 15:21:47 +0700

    feat(src): change component outer div to fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 const Hello = (props) => {
   console.log(props)
   return (
-  <div>
+  <>
     <p>Hello {props.name}, you are level {props.level}!</p>
-  </div>
+  </>
   )
-}  
+}
 
 const App = () => {
   const name = "Geralt of Rivia"
   const level = 48
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
       <Hello name="Rickyslash" level={2 + 6}/>
       <Hello name={name} level={level} />
-    </div>
+    </>
   )
 }
 
