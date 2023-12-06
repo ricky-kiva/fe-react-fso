2973b29ba3df5205a3bc3c5d9a981cf072654454
ricky-kiva <ricky.kiva@gmail.com>
2023-12-06 15:14:39 +0700

    feat(src): add `Hello` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
-
+const Hello = (props) => {
+  console.log(props)
   return (
-      <div>
-        <p>Hello world!</p>
-      </div>
+  <div>
+    <p>Hello {props.name}, you are level {props.level}!</p>
+  </div>
+  )
+}  
+
+const App = () => {
+  const name = "Geralt of Rivia"
+  const level = 48
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Rickyslash" level={2 + 6}/>
+      <Hello name={name} level={level} />
+    </div>
   )
 }
 
