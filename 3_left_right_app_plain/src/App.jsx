54be977217e26f3e0962ac79493846b500dce8b4
ricky-kiva <ricky.kiva@gmail.com>
2023-12-11 00:48:30 +0700

    feat(3-app): impl. Object Spread syntax to handlers

--- 3_left_right_app_plain/src/App.jsx
+++ 3_left_right_app_plain/src/App.jsx
@@ -5,21 +5,8 @@ const App = () => {
     left: 0, right: 0
   })
 
-  const handleLeftClick = () => {
-    const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right
-    }
-    setClicks(newClicks)
-  }
-
-  const handleRightClick = () => {
-    const newClicks = {
-      left: clicks.left,
-      right: clicks.right + 1
-    }
-    setClicks(newClicks)
-  }
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
