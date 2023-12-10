6df6669a6827f4cec16a7047a3c97d71e083b639
ricky-kiva <ricky.kiva@gmail.com>
2023-12-11 00:42:29 +0700

    feat(2-app): unify `useState()` Hook

--- 3_left_right_app_plain/src/App.jsx
+++ 3_left_right_app_plain/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react" 
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
-      {left}&nbsp;
-      <button onClick={() => setLeft(left + 1)}>Left</button>
-      <button onClick={() => setRight(right + 1)}>Right</button>
-      &nbsp;{right}
+      {clicks.left}&nbsp;
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      &nbsp;{clicks.right}
     </div>
   )
 }
