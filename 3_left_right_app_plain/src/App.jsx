84d5b5d5df04c3794f031648260e852cd12f1171
ricky-kiva <ricky.kiva@gmail.com>
2023-12-11 10:31:49 +0700

    feat(3-app): impl. all clicks accumulation

--- 3_left_right_app_plain/src/App.jsx
+++ 3_left_right_app_plain/src/App.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react" 
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
-  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('MCU')) // concat returns "new copy" of referenced array, instead of "mutating the existing array directly"
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('DCEU'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      {clicks.left}&nbsp;
-      <button onClick={handleLeftClick}>Left</button>
-      <button onClick={handleRightClick}>Right</button>
-      &nbsp;{clicks.right}
+      {left}&nbsp;
+      <button onClick={handleLeftClick}>MCU</button>
+      <button onClick={handleRightClick}>DCEU</button>
+      &nbsp;{right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
