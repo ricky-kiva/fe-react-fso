def06d480466fb34cd14e5972e0d7e03c24b067a
ricky-kiva <ricky.kiva@gmail.com>
2023-12-13 21:51:32 +0700

    feat(3-app): add `History` & `CustomButton` comp.

--- 3_left_right_app_plain/src/App.jsx
+++ 3_left_right_app_plain/src/App.jsx
@@ -7,26 +7,37 @@ const App = () => {
   const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
-    setAll(allClicks.concat('MCU')) // concat returns "new copy" of referenced array, instead of "mutating the existing array directly"
+    setAll(allClicks.concat('Shark')) // concat returns "new copy" of referenced array, instead of "mutating the existing array directly"
     const updatedLeft = left + 1 // we use this because react's "state update" is asynchronous (not immidiately, but "at some point")
     setLeft(updatedLeft)
     setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
-    setAll(allClicks.concat('DCEU'))
+    setAll(allClicks.concat('Dolphin'))
     const updatedRight = right + 1
     setRight(updatedRight)
     setTotal(left + updatedRight)
   }
 
+  const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return ( <p>The app is used by pressing the buttons</p> )
+    } else {
+      return ( <p>Button press history: {props.allClicks.join(' ')}</p> )
+    }
+  }
+
+  const CustomButton = ({handleClick, text }) =>
+    <button onClick={handleClick}>{text}</button>
+
   return (
     <div>
       {left}&nbsp;
-      <button onClick={handleLeftClick}>MCU</button>
-      <button onClick={handleRightClick}>DCEU</button>
+      <CustomButton handleClick={handleLeftClick} text="Shark" />
+      <CustomButton handleClick={handleRightClick} text="Dolphin" />
       &nbsp;{right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
       <p>Total: {total}</p>
     </div>
   )
